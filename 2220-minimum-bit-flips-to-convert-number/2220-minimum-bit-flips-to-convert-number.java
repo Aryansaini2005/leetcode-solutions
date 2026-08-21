ae80252e2517a2a383a718a14802838746d2a7d4
Aryansaini2005 <aaryansaini2005@gmail.com>
2026-08-21 08:12:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (73.19%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int ans = start^goal;
+        int cnt = 0;
+        while(ans > 0) {
+            if(ans%2 == 1) {
+                cnt++;
+            }
+            ans /= 2;
+        }
+        return cnt;
+    }
+}
